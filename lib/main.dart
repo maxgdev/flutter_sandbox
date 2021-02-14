be45398073aea4c8c58e515886601dce60d6f7cb
maxgdev <max@maxgdev.net>
2021-02-14 19:17:35 +0000

    Converted Stateless widget to stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,32 @@ void main() {
 }
 
 // Everythings a Widget. "AnyAppHere" main Widget
-class AnyApp extends StatelessWidget {
-  
+class AnyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return AnyAppState();
+  }
+}
+
+class AnyAppState extends State<AnyApp> {
+  var questionIndex = 0;
+
+  void answerQuestions() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print("Question Index: $questionIndex");  
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite Musician?',
+      'What\'s your favorite Film?',
+      'What\'s your favorite Food?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -17,20 +39,21 @@ class AnyApp extends StatelessWidget {
         body: Column(
           children: [
             Text(
-              "What is the Question?",
+              "${questions[questionIndex]}",
               style: TextStyle(fontSize: 25),
             ),
             RaisedButton(
               child: Text("Answer 1"),
-              onPressed: ()=> print("Button 1 pressed! "),
+              // onPressed: () => print("Button 1 pressed! "),
+              onPressed: answerQuestions,
             ),
             RaisedButton(
               child: Text("Answer 2"),
-              onPressed: ()=> print("Button 2 pressed! "),
+              onPressed: () => print("Button 2 pressed! "),
             ),
             RaisedButton(
               child: Text("Answer 3"),
-              onPressed: ()=> print("Button 3 pressed! "),
+              onPressed: () => print("Button 3 pressed! "),
             ),
           ],
         ),
