ece15603ad6bc9b33601fbd2a8372d3b6274fb5d
maxgdev <max@maxgdev.net>
2021-02-14 17:12:35 +0000

    Added function 'answerQuestions' to onPressed for buttons. No parentheses on onPressed: function call to avoid callback void error.

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,44 @@
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(AnyApp());
 }
 
 // Everythings a Widget. "AnyAppHere" main Widget
 class AnyApp extends StatelessWidget {
+  
+  void answerQuestions() {
+    print("Button pressed! ");
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('AppBar Title'),),
-        body: Column(children: [
-          Text("What is the Question?", style: TextStyle(fontSize: 25),),
-          RaisedButton(child: Text("Answer 1"), onPressed: null, ),
-          RaisedButton(child: Text("Answer 2"), onPressed: null, ),
-          RaisedButton(child: Text("Answer 3"), onPressed: null, ),
-        ],),
+        appBar: AppBar(
+          title: Text('AppBar Title'),
+        ),
+        body: Column(
+          children: [
+            Text(
+              "What is the Question?",
+              style: TextStyle(fontSize: 25),
+            ),
+            RaisedButton(
+              child: Text("Answer 1"),
+              onPressed: answerQuestions,
+            ),
+            RaisedButton(
+              child: Text("Answer 2"),
+              onPressed: answerQuestions,
+            ),
+            RaisedButton(
+              child: Text("Answer 3"),
+              onPressed: answerQuestions,
+            ),
+          ],
+        ),
       ),
     );
   }
