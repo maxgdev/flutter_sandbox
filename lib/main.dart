2f3203c84e302ffb3197444269172b62feb6a949
maxgdev <max@maxgdev.net>
2021-02-14 16:52:46 +0000

    Replaced body text wth Column and button widgets

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,12 @@ class AnyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('AppBar Title'),),
-        body: Text("Body Text"),
+        body: Column(children: [
+          Text("What is the Question?", style: TextStyle(fontSize: 25),),
+          RaisedButton(child: Text("Answer 1"), onPressed: null, ),
+          RaisedButton(child: Text("Answer 2"), onPressed: null, ),
+          RaisedButton(child: Text("Answer 3"), onPressed: null, ),
+        ],),
       ),
     );
   }
