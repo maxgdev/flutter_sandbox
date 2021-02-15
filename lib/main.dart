35d93d4f23155ca883b9dcba463e595da6346a1f
maxgdev <max@maxgdev.net>
2021-02-15 12:12:31 +0000

    Extracted answer button code to quiz.dart. also created result.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
 
 void main() {
   runApp(AnyApp());
@@ -18,54 +17,53 @@ class AnyApp extends StatefulWidget {
 class AnyAppState extends State<AnyApp> {
   var _questionsIndex = 0;
 
-  var questions = [
-  {
-    'questionText': 'What\'s your favorite Food?',
-    'answers': ['Rice', 'Eba', 'Soup', 'Yams']
-  },
-  {
-    'questionText': 'What\'s your favorite Car?',
-    'answers': ['BMW', 'Honda', 'Toyota', 'Porche']
-  },
-  {
-    'questionText': 'What\'s your favorite Film?',
-    'answers': ['Matrix', 'Black Panther', 'Bleach', 'Mandelorian']
-  },
-  {
-    'questionText': 'What\'s your favorite Musician?',
-    'answers': ['Burna Boy', 'Eddie Grant', 'Junior', 'Mice Paris']
-  }
-];
+  var _questions = [
+    {
+      'questionText': 'What\'s your favorite Food?',
+      'answers': ['Rice', 'Eba', 'Soup', 'Yams']
+    },
+    {
+      'questionText': 'What\'s your favorite Car?',
+      'answers': ['BMW', 'Honda', 'Toyota', 'Porche']
+    },
+    {
+      'questionText': 'What\'s your favorite Film?',
+      'answers': ['Matrix', 'Black Panther', 'Bleach', 'Mandelorian']
+    },
+    {
+      'questionText': 'What\'s your favorite Musician?',
+      'answers': ['Burna Boy', 'Eddie Grant', 'Junior', 'Mice Paris']
+    }
+  ];
 
   void _answerQuestions() {
     // if (_questionsIndex < questions.length-1) {
-      setState(() {
-        _questionsIndex = _questionsIndex + 1;
-      });
+    setState(() {
+      _questionsIndex = _questionsIndex + 1;
+    });
     // }
-    print("Question Index: $_questionsIndex of ${questions.length}");
+    print("Question Index: $_questionsIndex of ${_questions.length}");
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('AppBar Title'),
-        ),
-        body:  _questionsIndex < questions.length ? Column(
-          children: [
-            Question(
-              "${questions[_questionsIndex]['questionText']}",
-            ),
-            // Answer(_answerQuestions),
-            ...(questions[_questionsIndex]['answers'] as List<String>).map((answer) {
-              return Answer(_answerQuestions, answer);
-            }).toList(),
-          ],
-        ) :
-        Center(child:  Text("All Done!", style: TextStyle(fontSize: 30),),)
-      ),
+          appBar: AppBar(
+            title: Text('AppBar Title'),
+          ),
+          body: _questionsIndex < _questions.length
+              ? Quiz(
+                  answerQuestions: _answerQuestions, 
+                  questions:  _questions, 
+                  questionsIndex:  _questionsIndex
+                )
+              : Center(
+                  child: Text(
+                    "All Done!",
+                    style: TextStyle(fontSize: 30),
+                  ),
+                )),
     );
   }
 }
---
+++ lib/quiz.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import './question.dart';
+import './answer.dart';
+
+class Quiz extends StatelessWidget {
+  final Function answerQuestions;
+  final List<Map<String, Object>> questions;
+  final int questionsIndex;
+
+  Quiz(
+      {@required this.answerQuestions,
+      @required this.questions,
+      @required this.questionsIndex}
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Question(
+          "${questions[questionsIndex]['questionText']}",
+        ),
+        // Answer(_answerQuestions),
+        ...(questions[questionsIndex]['answers'] as List<String>).map((answer) {
+          return Answer(answerQuestions, answer);
+        }).toList(),
+      ],
+    );
+  }
+}
